59492902d1ee17f8e07884c8484b4de703dee386
Raj <i.gooner168@gmail.com>
2025-07-28 23:45:26 +0530

    Update App.jsx
    Changed in App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import UseState from './Hooks/UseState';
 function App() {
   return (
     <div className="App">
-      <h2>Welcome to React V18 and Hooks Concept !!</h2>
+      <h2>Create React App to Vite migration</h2>
       <UseState/>
       <hr/>
       <UseReducer />
